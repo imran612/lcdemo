10bfab56b435a609386bac95fd827542dfad48fc
imran612 <mohammei@STP7COR3029LTM.local>
2014-09-06 23:55:46 -0400

    09/03

--- LineClearanceDemo/www/view/ProcessQs.view.js
+++ LineClearanceDemo/www/view/ProcessQs.view.js
@@ -13,7 +13,7 @@ sap.ui.jsview("com.jbl.lnclr.tab.view.ProcessQs", {
                                         });
               
               
-              page.setCustomHeader(new sap.m.Bar("pageHdr3",{contentLeft: [new sap.m.Button({icon:"sap-icon://nav-back",press:oController.handleNavBack}),new sap.m.Image({src:"img/jabil-logo.jpg",height:"30px"}).addStyleClass("nyLogo")],contentRight:[new sap.m.Text({text:"Welcome Morgan"}).addStyleClass("welHdr"),new sap.m.Button({icon:"sap-icon://log"}) ]}));
+              page.setCustomHeader(new sap.m.Bar("pageHdr3",{contentLeft: [new sap.m.Button({icon:"sap-icon://nav-back",press:oController.handleNavBack}),new sap.m.Image({src:"img/nypro-ls-logo.png"}).addStyleClass("nyLogo")],contentRight:[new sap.m.Text({text:"Welcome Morgan"}).addStyleClass("welHdr"),new sap.m.Button({icon:"sap-icon://log"}) ]}));
               
               //display jbl logo
               page.addContent(new sap.m.Bar({contentLeft:new sap.m.Image({src:"img/jabil-logo.jpg",height:"20px"}).addStyleClass("jblLogo")}).addStyleClass("subHdrBar"));
@@ -22,35 +22,45 @@ sap.ui.jsview("com.jbl.lnclr.tab.view.ProcessQs", {
               var poQslPanel = new sap.m.Panel("poQslPanel",{width:"95%"});
               poQslPanel.addContent(new sap.m.Text({text:"Process Questionnaires"}).addStyleClass("eDataLabel"));
               
-              
-              var q1 = new sap.m.Panel({headerText:"Confirm ALL these items from the previous order have been removed and stored propery",expandable:true,expanded:false}).addStyleClass("procQsPanel");;
-              
+              //Question#1
+              var q1 = new sap.m.Panel({headerText:"Confirm ALL these items from the previous order have been removed and stored propery",expandable:true,expanded:true}).addStyleClass("procQsPanel");
               q1.addContent(sap.ui.xmlfragment("com.jbl.lnclr.tab.view.fragment.Questions",oController));
-              
               poQslPanel.addContent(q1);
               
-             /* lcDocProcPanel.addContent(new sap.m.Text("remark",{text:"Remark"}));
-              lcDocProcPanel.addContent(new sap.m.TextArea("remTxtArea"));
-              lcDocProcPanel.addContent(new sap.m.Text("esignlabel",{text:"e-Sign Operation and Quality"}));
+              //Question#2
+              var q2 = new sap.m.Panel({headerText:"Confirm retain samples have been collected",expandable:true,expanded:false}).addStyleClass("procQsPanelList");
+              q2.addContent(sap.ui.xmlfragment("com.jbl.lnclr.tab.view.fragment.Questions2",oController));
+              poQslPanel.addContent(q2);
               
-               var eSignPanel = new sap.m.Panel("eSignPanel");
-              eSignPanel.addContent(new sap.m.Text("eSignTxt1",{text:"E-Sign has to be executed by Operation first and then Quality"}));
-              eSignPanel.addContent(new sap.m.Text("eSignTxt2",{text:"OPERATION REP: I have carried out line clearence activity correctly"}));
-              eSignPanel.addContent(new sap.m.Text("eSignTxt3",{text:"QUALITY REP: I have carried out line clearance actvity correctly"}));
+              //Question#3
+              var q3 = new sap.m.Panel({headerText:"Confirm the following cleaning activities have been carried out",expandable:true,expanded:false}).addStyleClass("procQsPanelList");
+              q3.addContent(sap.ui.xmlfragment("com.jbl.lnclr.tab.view.fragment.Questions3",oController));
+              poQslPanel.addContent(q3);
               
-              var hrLyt=new sap.ui.layout.HorizontalLayout("hLyt");
-              hrLyt.addContent(new sap.m.Label({text:"e-Sign Operation & Quality:"}));
-              hrLyt.addContent(new sap.m.Input({type:sap.m.InputType.Text,valueHelpRequest:oController.handleESignDialog,showValueHelp:true,showSuggestion:true,}));
-              
-             eSignPanel.addContent(hrLyt);
-              
-              
-              
-              lcDocProcPanel.addContent(eSignPanel);*/
-              
+              //Question#4
+              var q4 = new sap.m.Panel({headerText:"Ensure compliance to the following",expandable:true,expanded:false}).addStyleClass("procQsPanelListFinal");
+              q4.addContent(sap.ui.xmlfragment("com.jbl.lnclr.tab.view.fragment.Questions4",oController));
+              poQslPanel.addContent(q4);
               page.addContent(poQslPanel);
               
+              //esign section
+              var eSignPanelTxt = new sap.m.Panel("eSignPanelTxt",{width:"95%"});
+              eSignPanelTxt.addContent(new sap.m.Text({text:"e-Sign Operation and Quality"}).addStyleClass("eDataLabel"));
+              page.addContent(eSignPanelTxt);
               
+              var eSignPanel = new sap.m.Panel("eSignPanel",{width:"90%"});
+              eSignPanel.addContent(new sap.m.Text({text:"E-Sign has to be executed by Operation first and then Quality"}).addStyleClass("eSignTxt1"));
+              eSignPanel.addContent(new sap.m.Text({text:"OPERATION REP", width:"100%"}).addStyleClass("eSignTxt2"));
+              eSignPanel.addContent(new sap.m.Text({text:"I have carried out line clearence activity correctly"}).addStyleClass("eSignTxt3"));
+               eSignPanel.addContent(new sap.m.Text({text:"QUALITY REP", width:"100%"}).addStyleClass("eSignTxt2"));
+              eSignPanel.addContent(new sap.m.Text({text:"I have carried out line clearance actvity correctly"}).addStyleClass("eSignTxt3"));
+              eSignPanel.addContent(new sap.m.Input({placeholder:"E-Sign Operation and Quality",type:sap.m.InputType.Text,valueHelpRequest:oController.handleESignDialog,showValueHelp:true,showSuggestion:true,width:"90%"}).addStyleClass("eSignFld"));
+              page.addContent(eSignPanel);
+              
+              //display cancel button
+              var cancelPanel = new sap.m.Panel("cancelPanel",{width:"95%"});
+              cancelPanel.addContent(new sap.m.Button({text:"Cancel",press:oController.handleCancel}).addStyleClass("canbtn"));
+               page.addContent(cancelPanel);
               return page;
               
               }
